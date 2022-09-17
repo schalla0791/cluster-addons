569a63fd4fd1fc271ad840027f98061dcd06c9e5
schalla <schalla0791@gmail.com>
2022-09-17 18:14:17 -0500

    update argocd

---
+++ argocd/open-argocd.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode && echo ""
+kubectl port-forward -n argocd svc/argocd-server 8443:443
+echo  https://localhost:8443
+
